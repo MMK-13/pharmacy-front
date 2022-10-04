ad727167a62367f8f59c27cf7fd7e5fc57db6d9b
MMK-13 <kantemamadou13@gmail.com>
2022-10-04 12:34:30 +0000

    Fixing of sidebar component

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,7 +3,32 @@
 		<div class="sidebar" v-if="visibility">
 			<UserInfo :fullname="user.fullname" :phone="user.phone" />
 			<div class="divider"></div>
-			
+			<div class="sidebar__options">
+				<div class="sidebar__option">
+					<font-awesome-icon icon="fa-solid fa-capsules" />
+					<span class="sidebar__option-text">Online pharmacy</span>
+				</div>
+				<div class="sidebar__option">
+					<font-awesome-icon icon="fa-solid fa-ellipsis-vertical" />
+					<span class="sidebar__option-text">All services</span>
+				</div>
+				<div class="sidebar__option">
+					<font-awesome-icon icon="fa-solid fa-info" />
+					<span class="sidebar__option-text">About us</span>
+				</div>
+			</div>
+			<div class="divider"></div>
+			<div class="sidebar__options">
+				<div class="sidebar__option">
+					<font-awesome-icon icon="fa-solid fa-share-nodes" />
+					<span class="sidebar__option-text">Tell your friend</span>
+				</div>
+				<div class="sidebar__option">
+					<font-awesome-icon icon="fa-regular fa-message" />
+					<span class="sidebar__option-text">Feedback & contact us</span>
+				</div>
+				
+			</div>
 		</div>
 	</Transition>
 </template>
@@ -35,13 +60,13 @@
 	}
 	.sidebar-enter-from,
 	.sidebar-leave-to {
-		transform: translateX(-275px);
+		transform: translateX(-296px);
 	}
 	.sidebar {
 		position: fixed;
 		top: 0;
 		left: 0;
-		width: 275px;
+		width: 296px;
 		height: 100%;
 		z-index: 10;
 		background-color: #FFF;
@@ -49,5 +74,30 @@
 	.divider {
 		border-bottom: 1px solid #E5E5E5;
 		padding: 8px 0;
+	}
+	.sidebar__options {
+		margin-top: 16px;
+		display: flex;
+		flex-direction: column;
+	}
+	.sidebar__option {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		padding: 16px 32px;
+	}
+	.sidebar__option:hover {
+		transition: background-color 1s ease;
+		background-color: #F1F1F1;
+	}
+	.sidebar__option-text {
+		font-size: 16px;
+		margin-left: 8px;
+		color: #807C7C;
+	}
+	.sidebar__option svg {
+		color: #00A651;
+		width: 22px;
+		height: 22px;
 	}
 </style>
