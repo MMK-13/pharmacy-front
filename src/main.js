77a0bde7f52ded6338f0903b50c2d53798e7e046
MMK-13 <kantemamadou13@gmail.com>
2022-10-05 18:53:17 +0000

    Creating router file

--- src/main.js
+++ src/main.js
@@ -1,27 +1,8 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import router from './router'
 import App from './App.vue'
-import SplashScreen from './pages/SplashScreen.vue'
-import StartingPage from './pages/StartingPage.vue'
-import RegisterPage from './pages/RegisterPage.vue'
-import ConfimationPhone from './pages/ConfimationPhone.vue'
-import HomePage from './pages/HomePage.vue'
-import AccountPage from './pages/AccountPage.vue'
-
-const router = createRouter({
-	history: createWebHistory(),
-	linkActiveClass: 'rooter-link-active',
-	routes: [
-		{path: '/', component: SplashScreen, },
-		{path: '/starting', component: StartingPage},
-		{path: '/register', component: RegisterPage},
-		{path: '/confirmation', component: ConfimationPhone},
-		{path: '/home', name: 'home', component: HomePage},
-		{path: '/account', name: 'account', component: AccountPage},
-	]
-})
 
 // import { fas } from '@fortawesome/pro-solid-svg-icons'
 // import { fab } from '@fortawesome/free-brands-svg-icons'
---
+++ src/router.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import SplashScreen from './pages/SplashScreen.vue'
+import StartingPage from './pages/StartingPage.vue'
+import RegisterPage from './pages/RegisterPage.vue'
+import ConfimationPhone from './pages/ConfimationPhone.vue'
+import HomePage from './pages/HomePage.vue'
+import AccountPage from './pages/AccountPage.vue'
+
+const router = createRouter({
+	history: createWebHistory(),
+	linkActiveClass: 'rooter-link-active',
+	routes: [
+		{path: '/', component: SplashScreen, },
+		{path: '/starting', component: StartingPage},
+		{path: '/register', component: RegisterPage},
+		{path: '/confirmation', component: ConfimationPhone},
+		{path: '/home', name: 'home', component: HomePage},
+		{path: '/account', name: 'account', component: AccountPage},
+	]
+})
+export default router
